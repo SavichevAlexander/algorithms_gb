6f8497928f498b5dedd0e8e68de8a45ad930e3ef
ACupOfWhiskey <mia.nishida@gmail.com>
2023-07-19 19:24:24 +0300

    complete 2nd lesson hw

---
+++ 2nd_lesson_hw/main.java
@@ -0,0 +1,17 @@
+public class main {
+    public static void main(String[] args) {
+        linkedlist list = new linkedlist();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+     
+       
+        list.print();
+        list.revert();
+        list.print();
+       
+    }  
+}
